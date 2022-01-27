e7ec6c4c7bc3819ae53a97e864bf1847065a4d7e
Hayk Hovhannisyan <haik.hovhanisyan@gmail.com>
2022-01-27 21:34:35 +0400

    Add project skeleton.

---
+++ src/main/java/com/hhovhann/optimisationservice/OptimisationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hhovhann.optimisationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimisationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhovhann/optimisationservice/OptimisationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhovhann.optimisationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
